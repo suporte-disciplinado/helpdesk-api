555d47f6aa59f9dfdd7a8fffa1df91bf3fefac26
Alberto Fontenelle Pluecker <albertopluecker@gmail.com>
2025-02-23 11:16:59 +0100

    email/password authentication

---
+++ src/main/java/com/suportedisciplinado/api/controller/AuthController.java
@@ -0,0 +1,42 @@
+package com.suportedisciplinado.api.controller;
+
+import com.suportedisciplinado.api.model.User;
+import com.suportedisciplinado.api.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @GetMapping("/me")
+    public UserDetails currentUser(@AuthenticationPrincipal UserDetails user) {
+      return user;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            return ResponseEntity.badRequest().body("Email already exists");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+}
