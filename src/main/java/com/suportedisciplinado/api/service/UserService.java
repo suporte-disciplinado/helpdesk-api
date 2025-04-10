6f14c589813ee48285a2609aaa2ff6ed43c05347
Alberto Cesário <alberto.cesario@al.infnet.edu.br>
2025-04-09 23:41:43 -0300

    UserController

---
+++ src/main/java/com/suportedisciplinado/api/service/UserService.java
@@ -0,0 +1,50 @@
+package com.suportedisciplinado.api.service;
+
+import com.suportedisciplinado.api.model.User;
+import com.suportedisciplinado.api.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+
+    public ResponseEntity<Optional<User>> getUserById(Long id) {
+        return ResponseEntity.ok(userRepository.findById(id));
+    }
+
+    public ResponseEntity<User> createUser(User user) {
+        return ResponseEntity.ok(userRepository.saveAndFlush(user));
+    }
+
+    public ResponseEntity<User> updateUser(User user) {
+        Optional<User> findUser = userRepository.findById(user.getId());
+        if (findUser.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        User userToUpdate = findUser.get();
+        userToUpdate.setName(user.getName());
+        userToUpdate.setEmail(user.getEmail());
+
+        User updatedUser = userRepository.saveAndFlush(userToUpdate);
+        return ResponseEntity.ok(userRepository.saveAndFlush(updatedUser));
+    }
+
+    public ResponseEntity<String> deleteUser(Long id) {
+        userRepository.deleteById(id);
+        return ResponseEntity.ok("User deleted successfully!");
+    }
+}
