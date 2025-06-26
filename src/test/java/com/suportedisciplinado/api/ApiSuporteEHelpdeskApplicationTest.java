69938bbc870226d3b6f9bbb96c9fa3574f75c72e
Alberto Cesário <alberto.cesario@al.infnet.edu.br>
2025-06-26 09:59:18 -0300

    Profile dev ativado nos testes

--- src/test/java/com/suportedisciplinado/api/ApiSuporteEHelpdeskApplicationTest.java
+++ src/test/java/com/suportedisciplinado/api/ApiSuporteEHelpdeskApplicationTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("dev")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiSuporteEHelpdeskApplicationTest {
 
